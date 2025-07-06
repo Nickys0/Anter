89e8e96b24f76b84b67f304698d1c39ea09f5e91
Nicky <nicholasscardino07@gmail.com>
2025-07-06 18:49:37 +0200

    Fixes: Avoiding redundacy in function ArgtpToString + New function

--- arg.go
+++ arg.go
@@ -15,17 +15,20 @@ const(
 	ARGF_NONE = -1
 )
 
+// TODO:
+// This function should be a part 
+// of the Arg structure
 func ArgtpToString(tp int) string{
 	switch tp{
-		case ARGTP_EOA: 		return "EOA"	
-		case ARGTP_COMMAND: 	return "COMMAND"	
-		case ARGTP_FLAG: 		return "FLAG"	
-		case ARGTP_VALUE: 		return "VALUE"	
-		case ARGTP_UNKNOWN:		return "UNKNOWN"
-		case ARGTP_BINPATH:		return "BINPATH"
-	default:
-			return "UNKNOWN"
+	case ARGTP_EOA: 		return "EOA"	
+	case ARGTP_COMMAND: 	return "COMMAND"	
+	case ARGTP_FLAG: 		return "FLAG"	
+	case ARGTP_VALUE: 		return "VALUE"	
+	case ARGTP_BINPATH:		return "BINPATH"
 	}
+	
+	// default + case ARGTP_UNKNOWN
+	return "UNKNOWN"
 }
 
 type Arg struct{
@@ -104,6 +107,12 @@ func argFlag_Value(_str string, r_idx, a_idx int) Arg{
 	return Arg{ tp: ARGTP_FLAG, str: _str, 
 				r_indx: r_idx, a_indx: a_idx, 
 				argf: FTYPE_VALUE}
+}
+
+func argInvalid( ) Arg {
+	return Arg{ tp: ARGTP_UNKNOWN, str: "", 
+				r_indx: -1, a_indx: -1, 
+				argf: ARGF_NONE, }
 }
 
 func argEOA(idx int) Arg{
